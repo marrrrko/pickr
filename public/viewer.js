5a79ac6effe8eedc56c3c34af527372a02562948
Mark Carrier <mark@otomotion.com>
2017-11-30 13:22:51 +0000

    Adds automatic page reload every hour to see impact on browser issues

--- public/viewer.js
+++ public/viewer.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   window.desiredSecondsPerPicture = 60
   window.minimumPauseBetweenRequests = 10 //give the server (and the cpu) a little breather
   window.timeOfLastPhotoUpdate = new Date(0);
+  window.pageLoadTime = new Date();
   window.debug = false;
   sendLogToServer('info','Client starting with ' + desiredSecondsPerPicture + 's/' + minimumPauseBetweenRequests + 's delays configured')
   getNextPhoto()
@@ -30,46 +31,63 @@ toastr.options = {
 }
 
 function getNextPhoto() {
-  try {
-    var imgNode = $('<img class="the-photo" alt="If you can read this, something went terribly wrong.">')
-    var requestTime = new Date()
-    $.getJSON("feed?" + requestTime.getTime(), function( data ) {    
-      imgNode.attr('src','data:image/jpeg;base64,' + data.photoData);
-      var datetime = moment(new Date(parseInt(data.photoExif.tags.DateTimeOriginal) * 1000)).format('MMMM Do YYYY');
-      var orientation = data.photoExif.tags.Orientation;
-      var label = data.photoInfo.photos.photo[0].title;
-      var msg =  label + '<br />' + datetime + '<br />Orientation: ' + orientation + '<br />Served at: ' + (new Date()).toLocaleTimeString();
-      sendLogToServer('info',msg)
-      
-      var neededRotation = 0;
-      if(orientation == 8) {
-        neededRotation = 270; 
-      } else if (orientation == 6)  {
-        neededRotation = 90;
-      }
-      
-      var secondsElapsedSinceLastUpdate = Math.floor((new Date() - timeOfLastPhotoUpdate) / 1000) 
-      var secondsMissingToMatchDesiredWait = desiredSecondsPerPicture - secondsElapsedSinceLastUpdate
-      
-      var timeToWait = Math.max(secondsMissingToMatchDesiredWait,minimumPauseBetweenRequests)
-      
-      if(secondsElapsedSinceLastUpdate > 10000000) //first load
-        timeToWait = 0 
-      
-      console.log('We\'ll wait ' + timeToWait + ' seconds before switching')
-      schedulePhotoUpdate(imgNode, { caption: label, dateLabel: datetime }, neededRotation, timeToWait * 1000, msg)
-    }).fail(function( jqxhr, textStatus, error ) {
-        var photoRequestError = jqxhr.status + ": " + textStatus + ", " + error;
-        if(jqxhr.status == 404) {
-          console.log( "No photo found.  Retrying in 20 seconds.");
-          schedulePhotoLoad(20000);
-        } else {
-          schedulePhotoLoad(30000,'HTTP error while retrieving photo: ' + photoRequestError);
+  
+  let now = new Date();
+  
+  var secondsElapsedSinceLastUpdate = Math.floor((now - timeOfLastPhotoUpdate) / 1000) 
+  var secondsMissingToMatchDesiredWait = desiredSecondsPerPicture - secondsElapsedSinceLastUpdate
+        
+  var timeToWait = Math.max(secondsMissingToMatchDesiredWait,minimumPauseBetweenRequests)
+        
+  if(secondsElapsedSinceLastUpdate > 10000000) //first load
+      timeToWait = 0 
+  
+  let minutesSincePageLoaded = (now - window.pageLoadTime) / (1000*60);
+  
+  if (minutesSincePageLoaded >= 60) {
+    schedulePageReload(timeToWait * 1000);
+  } else {
+    try {
+      var imgNode = $('<img class="the-photo" alt="If you can read this, something went terribly wrong.">')
+      var requestTime = new Date()
+      $.getJSON("feed?" + requestTime.getTime(), function( data ) {    
+        imgNode.attr('src','data:image/jpeg;base64,' + data.photoData);
+        var datetime = moment(new Date(parseInt(data.photoExif.tags.DateTimeOriginal) * 1000)).format('MMMM Do YYYY');
+        var orientation = data.photoExif.tags.Orientation;
+        var label = data.photoInfo.photos.photo[0].title;
+        var msg =  label + '<br />' + datetime + '<br />Orientation: ' + orientation + '<br />Served at: ' + (new Date()).toLocaleTimeString();
+        sendLogToServer('info',msg)
+        
+        var neededRotation = 0;
+        if(orientation == 8) {
+          neededRotation = 270; 
+        } else if (orientation == 6)  {
+          neededRotation = 90;
         }
-    });
-  } catch(err) {
-    schedulePhotoLoad(30000,'Unknown error while retrieving photo: ' + err);
+        
+        console.log('We\'ll wait ' + timeToWait + ' seconds before switching')
+        schedulePhotoUpdate(imgNode, { caption: label, dateLabel: datetime }, neededRotation, timeToWait * 1000, msg)
+      }).fail(function( jqxhr, textStatus, error ) {
+          var photoRequestError = jqxhr.status + ": " + textStatus + ", " + error;
+          if(jqxhr.status == 404) {
+            console.log( "No photo found.  Retrying in 20 seconds.");
+            schedulePhotoLoad(20000);
+          } else {
+            schedulePhotoLoad(30000,'HTTP error while retrieving photo: ' + photoRequestError);
+          }
+      });
+    } catch(err) {
+      schedulePhotoLoad(30000,'Unknown error while retrieving photo: ' + err);
+    }
   }
+}
+
+function schedulePageReload(delay) {
+  sendLogToServer('info','Client will reload page in ' + delay);
+  setTimeout(function() {
+    sendLogToServer('info','Client reloading page');
+    window.location.reload();
+  },delay)
 }
 
 function schedulePhotoLoad(delay, errMessage)  {
@@ -84,7 +102,6 @@ function schedulePhotoLoad(delay, errMessage)  {
     toastr["info"]("Retrying in " + Math.floor(delay/1000) + "s")
   }
 }
-
 
 function schedulePhotoUpdate(imgNode, imgInfo, rotation, delay, msg)  {
     
